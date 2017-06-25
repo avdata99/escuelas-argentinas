cb17b938562aa0616ca7682bc570c02c424783f1
Andrés Vázquez <andres@data99.com.ar>
2017-06-25 17:21:52 -0300

    fix shp script

--- scripts/get-shapes.sh
+++ scripts/get-shapes.sh
@@ -1,7 +1,11 @@
 # original manu https://github.com/jazzido/dondevoto
 # Ojo que pesa mas de 13GBs
 
-for i in `seq 1 1000 60000`;
-  do ogr2ogr -append -f "ESRI Shapefile" escuelas WFS:"http://www.mapaeducativo.edu.ar/geoserver/ows?service=wfs&version=1.0.0&sortBy=gid&startIndex=$i" men:escuelas_oferta;
-done
 
+# de un saque sin paginar
+ogr2ogr -append -f "ESRI Shapefile" escuelas WFS:"http://www.mapaeducativo.edu.ar/geoserver/ows?service=wfs&version=1.0.0&sortBy=gid&startIndex=1&maxFeatures=80000" men:escuelas_oferta;
+
+# paginando
+# for i in `seq 1 1000 60000`;
+#  do ogr2ogr -append -f "ESRI Shapefile" escuelas WFS:"http://www.mapaeducativo.edu.ar/geoserver/ows?service=wfs&version=1.0.0&sortBy=gid&startIndex=$i&maxFeatures=10000" men:escuelas_oferta;
+# done
